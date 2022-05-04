b0a84643fc1e8b8b3718fa3362635fad7eeb07fb
imskanand <the.anand3099@gmail.com>
2022-05-04 22:21:40 +0530

    Created a http server

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const fs = require('fs')
+const http = require('http')
+
+const server = http.createServer((req,res) => {
+  console.log('Joined')
+})
+
+server.listen(3001)
 fs.writeFileSync('myText.txt', 'Hello, Chittering App')
