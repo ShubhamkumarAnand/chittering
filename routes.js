d5f9744ef6e912f1a27a9b366a2266a00c3c2351
imskanand <the.anand3099@gmail.com>
2022-05-04 23:13:52 +0530

    Handling POST and GET Request

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const displayContent = (req, res) => {
 
   const url = req.url
+  const method = req.method
 
   if (url == '/profile') {
     res.setHeader('Content-Type', 'text/html')
@@ -9,6 +10,23 @@ const displayContent = (req, res) => {
   } else if (url == '/') {
     res.setHeader('Content-Type', 'text/html')
     res.write('<h1>cheeseburger</h1>')
+    return res.end()
+  } else if (url == '/settings' && method=='POST') {
+    res.setHeader('Content-Type', 'text/html')
+    res.write(`
+      <h1> Form Was submitted Boy</h1>
+      `
+    )
+    return res.end()
+  } else if (url == '/settings') {
+    res.setHeader('Content-Type', 'text/html')
+    res.write(`
+      <h1> settings</h1>
+      <form action='/settings' method='POST'> 
+        <input type='text'/>
+        <button type='submit'>submit</button>
+      </form>`
+    )
     return res.end()
   }
 
