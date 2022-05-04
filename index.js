327c434eeb540cf73bd0312c9408f88d7b9a82ba
imskanand <the.anand3099@gmail.com>
2022-05-04 22:40:24 +0530

    Adding more pages to the application

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const fs = require('fs')
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type','text/html')
-  res.write('cheeseburger')
+
+  const url = req.url
+
+  if (url == '/profile') {
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<h2> This is a Profile Page </h2>')
+    return res.end()
+  } else if (url == '/') {
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<h1>cheeseburger</h1>')
+    return res.end()
+  }
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<h1>Page Not Found</h1>')
   res.end()
+
   console.log('Joined')
 })
 
